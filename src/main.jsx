86b2acf55d76352a6af793a1589f2a1b52e0ce95
Pulkit-2002 <141421176+Pulkit-2002@users.noreply.github.com>
2025-04-23 16:44:15 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // If you are using Tailwind, index.css includes @tailwind directives
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
